e88dbe3f41d9f48631962ea33d517ee9740c99d4
Kratoxx <kratoxx@pop-os.localdomain>
2022-03-15 19:00:44 -0600

    m

--- app/app.js
+++ app/app.js
@@ -3,13 +3,21 @@ import RadSideDrawer from 'nativescript-ui-sidedrawer/vue';
 import Vuex from 'vuex';
 import store from './store/index';
 import router from './router/index';
-Vue.use(Vuex)
-Vue.use(RadSideDrawer)
+import {TNSFontIcon, fonticon} from 'nativescript-fonticon';
+
+
 
 import App from './components/App';
 import { onErrorResumeNext } from 'rxjs';
 
+TNSFontIcon.debug = true;
+TNSFontIcon.loadCss();
+
 Vue.config.silent = !__DEV__
+
+Vue.use(Vuex)
+Vue.use(RadSideDrawer)
+Vue.filter('fonticon', fonticon);
 
 new Vue({
   store,
